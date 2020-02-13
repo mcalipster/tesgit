73342a4cff4630c20baadc1a57aee39fde3740f6
mcalipster <aridwanrifani@gmail.com>
2020-02-13 11:23:26 +0700

    commit1

---
+++ 0.py
@@ -0,0 +1,15 @@
+# version 1.0
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+#catatan github
+- git init
+- git add .
+- git commit -m "blalaa"
+
+- CHECKOUT commit => read_only
+- REVERT commit => modifikasi bahkan bisa menghapus beberapa commit yg lalu
+- RESET commit => mereset commit ke poin yg ditentukan
